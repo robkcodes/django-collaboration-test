71425c7239a16a43b210b76880b4577a32a95f18
Rob Killick <robkillick@Robs-MBP-2.home>
2024-04-08 21:54:11 +0100

    set debug to false

--- djangoplayground/settings.py
+++ djangoplayground/settings.py
@@ -30,9 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEBUG' in os.environ
-
-DEBUG = True
+DEBUG = 'DEBUG' in os.environ
 
 ALLOWED_HOSTS = ['.herokuapp.com']
 
