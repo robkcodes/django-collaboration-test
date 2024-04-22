cb69369f8ff06d810e26e6511be1892b6ee111b0
Rob Killick <robkillick@Robs-MBP-2.home>
2024-04-22 12:00:35 +0100

    feat: add post model and form

---
+++ home/forms.py
@@ -0,0 +1,11 @@
+from .models import Post
+from django import forms
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = (
+            "title",
+            "content",
+        )
--- home/views.py
+++ home/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import PostForm
 
 
 def home_page(request):
     """
     Returns Hello Message
     """
-    return render(request, "home/index.html")
+
+    post_form = PostForm()
+    return render(
+        request,
+        "home/index.html",
+        {
+            "post_form": post_form,
+        },
+    )
